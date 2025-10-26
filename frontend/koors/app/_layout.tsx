cf3aab136e74ff49deeed66140a81d5a82cef9f1
Patrick <patrickzambonie@gmail.com>
2025-10-26 03:37:50 -0400

    Frontend 2

---
+++ frontend/koors/app/_layout.tsx
@@ -0,0 +1,43 @@
+import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
+import {StatusBar} from 'expo-status-bar';
+import {AppRegistry, Text} from 'react-native';
+import 'react-native-reanimated';
+
+import {useColorScheme} from '@/hooks/use-color-scheme';
+import HealthKit, {
+  AuthorizationRequestStatus,
+  UpdateFrequency,
+  useHealthkitAuthorization
+} from "@kingstinct/react-native-healthkit";
+import {useEffect} from "react";
+import HomeScreen from "@/app/index";
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [authStatus, requestAuthorization] = useHealthkitAuthorization(["HKQuantityTypeIdentifierHeartRate"]);
+
+  useEffect(() => {
+    requestAuthorization();
+
+    HealthKit.enableBackgroundDelivery(
+      "HKCategoryTypeIdentifierHighHeartRateEvent",
+      UpdateFrequency.immediate,
+    )
+
+    HealthKit.enableBackgroundDelivery(
+      "HKQuantityTypeIdentifierHeartRate",
+      UpdateFrequency.immediate,
+    )
+  }, [authStatus, requestAuthorization]);
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        {authStatus !== AuthorizationRequestStatus.unnecessary ? (
+          <Text>Please allow Koors to read your heart rate</Text>
+        ) : (
+          <HomeScreen />
+        )}
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
---
+++ frontend/koors/app/index.tsx
@@ -0,0 +1,321 @@
+import {Animated, Text, StyleSheet, View} from 'react-native';
+import {useCallback, useEffect, useMemo, useRef, useState} from "react";
+import HealthKit, {
+  useMostRecentQuantitySample,
+} from "@kingstinct/react-native-healthkit";
+import KoorsButton from "@/components/KoorsButton";
+import {startAdvertising, stopAdvertising} from "munim-bluetooth-peripheral";
+import Voice from '@react-native-voice/voice';
+
+export default function HomeScreen() {
+  const [heartRate, setHeartRate] = useState(useMostRecentQuantitySample("HKQuantityTypeIdentifierHeartRate")?.quantity ?? 0);
+  const [isListening, setIsListening] = useState(false);
+  const [showVoiceUI, setShowVoiceUI] = useState(false);
+  const [liveVoiceTranscription, setLiveVoiceTranscription] = useState<string>("");
+  const [commandRecognized, setCommandRecognized] = useState<boolean | null>(null)
+  const transcriptionRef = useRef("");
+
+  const backdropRef = useRef(new Animated.Value(0)).current
+  const commandColorRef = useRef(new Animated.Value(0)).current
+  const commandAnimRef = useRef(new Animated.Value(1)).current
+
+  // Listen live while the app is open
+  useEffect(() => {
+    const id = setInterval(async () => {
+      try {
+        const sample = await HealthKit.getMostRecentQuantitySample("HKQuantityTypeIdentifierHeartRate")
+        if (sample) {
+          setHeartRate(sample.quantity)
+        }
+      } catch (e) {
+        console.error("Error getting most recent sample:", e)
+      }
+    }, 1000 * 5);
+
+    return () => {
+      clearInterval(id)
+    }
+  }, []);
+
+  const beatingHeartAnim = useRef(new Animated.Value(1)).current
+  useEffect(() => {
+    Animated.loop(
+      Animated.spring(beatingHeartAnim, {
+        toValue: 1.1,
+        useNativeDriver: true,
+      }),
+      {
+        iterations: -1,
+      },
+    ).start()
+  }, []);
+
+  const koorsState: 'searching' | 'ready' = useMemo(() => {
+    if (heartRate >= 180) {
+      return 'searching'
+    } else {
+      return 'ready'
+    }
+  }, [heartRate])
+
+  const [koorsStateText, koorsStateColor] = useMemo(() => {
+    switch (koorsState) {
+      case 'ready':
+        return ['Ready', '#2da400']
+      case 'searching':
+        return ['Searching...', '#d5af00']
+    }
+  }, [koorsState])
+
+  useEffect(() => {
+    Voice.onSpeechResults = (event) => {
+      console.log('Speech results:', event.value);
+      if (event.value && event.value.length > 0) {
+        const command = event.value[0].toLowerCase();
+        console.log("COMMAND: " + command);
+        setLiveVoiceTranscription(command)
+        transcriptionRef.current = command
+      }
+    };
+
+    Voice.onSpeechEnd = () => {
+      console.log('Speech ended');
+    };
+
+    Voice.onSpeechError = (error) => {
+      console.error('Speech error:', error);
+      setIsListening(false);
+    };
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const startListening = async () => {
+    try {
+      await Voice.start('en-US');
+      setIsListening(true);
+      setShowVoiceUI(true);
+      setCommandRecognized(null)
+      commandColorRef.setValue(0)
+      commandAnimRef.setValue(1)
+      setLiveVoiceTranscription("")
+
+      Animated.timing(backdropRef, {
+        toValue: 1,
+        duration: 80,
+        useNativeDriver: true,
+      }).start()
+    } catch (error) {
+      console.error('Error starting voice recognition:', error);
+    }
+  };
+
+  const stopListening = async () => {
+    try {
+      setTimeout(async () => {
+        await Voice.stop();
+      }, 500)
+    } catch (error) {
+      console.error('Error stopping voice recognition:', error);
+    }
+
+    setIsListening(false);
+    setTimeout(() => processCommand(), 750)
+  };
+
+  useEffect(() => {
+    startAdvertising({
+      serviceUUIDs: ['180D', '180F'],
+      localName: 'Koors Beacon',
+      manufacturerData: '0102030405',
+    });
+
+    return () => {
+      stopAdvertising()
+    }
+  }, [])
+
+  const fadeOutVoiceUI = () => {
+    Animated.timing(backdropRef, {
+      toValue: 0,
+      duration: 80,
+      useNativeDriver: true,
+    }).start(() => {
+      setShowVoiceUI(false)
+    })
+  }
+
+  const processCommand = () => {
+    console.log("FINAL COMMAND => " + transcriptionRef.current)
+
+    // Match up command
+    const command = transcriptionRef.current.toLowerCase()
+    let realizedCommand = null
+    if (command.includes("flip")) {
+      realizedCommand = "flip"
+    } else if (command.includes("help")) {
+      realizedCommand = "help"
+    }
+
+    if (realizedCommand) {
+      setCommandRecognized(true)
+    } else {
+      setCommandRecognized(false)
+    }
+
+    Animated.timing(commandColorRef, {
+      toValue: 1,
+      duration: 80,
+      useNativeDriver: false,
+    }).start(() => {
+      setTimeout(fadeOutVoiceUI, 500)
+    })
+
+    commandAnimRef.setValue(1.1)
+    Animated.timing(commandAnimRef, {
+      toValue: 1,
+      duration: 80,
+      useNativeDriver: false,
+    }).start()
+  }
+
+  const commandColor = useMemo(() => {
+    if (commandRecognized === true) {
+      return '#2da400'
+    } else if (commandRecognized === false) {
+      return '#d50000'
+    } else {
+      return '#ffffff'
+    }
+  }, [commandRecognized])
+
+  return (
+      <View style={styles.centerContainer}>
+        <View style={styles.container}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>🐕 koors.</Text>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <Text style={styles.subLabel}>Last Heartrate</Text>
+            <View style={styles.heartRateTextContainer}>
+              <Animated.Text style={[styles.beatingHeart, {transform: [{scale: beatingHeartAnim}]}]}>❤️</Animated.Text>
+              <Text style={styles.valueText}>{Math.round(heartRate)} BPM</Text>
+            </View>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <Text style={styles.subLabel}>Koors is...</Text>
+            <Text style={[styles.valueText, {color: koorsStateColor}]}>{koorsStateText}</Text>
+          </View>
+
+          <View style={styles.buttons}>
+            <KoorsButton>Koors, IM DYING!</KoorsButton>
+            <KoorsButton
+              variant="transparent"
+              onPressIn={startListening}
+              onPressOut={stopListening}
+            >
+              Koors, LISTEN!
+            </KoorsButton>
+          </View>
+        </View>
+
+        {showVoiceUI && (
+          <Animated.View style={[styles.backdrop, { opacity: backdropRef }]}>
+            <Animated.Text
+              style={[
+                styles.transcriptionText,
+                {
+                  color: commandColorRef.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: ['#ffffff', commandColor]
+                  }),
+                  transform: [{scale: commandAnimRef}]
+                }
+              ]}
+            >
+              {liveVoiceTranscription}
+            </Animated.Text>
+          </Animated.View>
+        )}
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  centerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    gap: 30,
+    width: 275,
+  },
+
+  titleContainer: {
+    transformOrigin: [0, 0, 0],
+    transform: [{scaleX: 1.25}]
+  },
+
+  title: {
+    fontSize: 64,
+    fontWeight: 'bold'
+  },
+
+  infoContainer: {
+    alignItems: 'flex-start',
+  },
+
+  subLabel: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#555'
+  },
+
+  heartRateTextContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  valueText: {
+    fontWeight: 'bold',
+    fontSize: 42,
+  },
+
+  beatingHeart: {
+    fontSize: 42,
+    transform: [{scale: 2}]
+  },
+
+  buttons: {
+    gap: 10
+  },
+
+  backdrop: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 1,
+    padding: 50,
+  },
+
+  transcriptionText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  }
+});
